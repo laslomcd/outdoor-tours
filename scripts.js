d07bb8cdd40633faf13456bac50931a7d88bcfb6
ryan <laslomcd@gmail.com>
2021-05-10 10:13:04 -0400

    restarted project

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 const selectElement = (s) => document.querySelector(s);
 
 /* Open Menu */
-selectElement(".open").addEventListener("click", function () {
+selectElement(".open").addEventListener("click", () => {
 	selectElement(".nav-list").classList.add("active");
 });
 
-selectElement(".close").addEventListener("click", function () {
+selectElement(".close").addEventListener("click", () => {
 	selectElement(".nav-list").classList.remove("active");
 });
